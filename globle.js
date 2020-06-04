4893e066a127c53bc7ecd554ef6ffae4637f25aa
Edward Tang <edwardtang@Edwards-MacBook-Pro.local>
2020-06-04 02:13:45 -0700

    visualize

---
+++ globle.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const path = require("path")
+const ejs = require("ejs")
+const fs = require("fs")
+
+// const config = require('static/data/ECS152A/chapter1.json')
+
+const app = express();
+const port = 4000;
+
+function escapeSpecialChars(jsonString) {
+    return jsonString
+		.replace(/\\n/g, "\\n")
+        .replace(/\\'/g, "\\'")
+        .replace(/\\"/g, '\\"')
+        .replace(/\\&/g, "\\&")
+        .replace(/\\r/g, "\\r")
+        .replace(/\\t/g, "\\t")
+        .replace(/\\b/g, "\\b")
+        .replace(/\\f/g, "\\f");
+
+}
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "./solution/static/views"))
+app.set("views", path.join(__dirname, "./wlan/static/views"))
+
+app.use(express.static(path.join(__dirname, "./solution")))
+app.use(express.static(path.join(__dirname, "./wlan")))
+
+app.get("/", (request, response) => {
+	var dataFile = fs.readFileSync(path.join(__dirname, "./solution/static/data/ECS152A/solution.json"), 'utf8');
+	dataFile = JSON.stringify(dataFile)
+	dataFile = dataFile.replace(/\\n/g, "\\n")
+					   .replace(/\\'/g, "\\'")
+					   .replace(/\\"/g, '\\"')
+					   .replace(/\\&/g, "\\&")
+					   .replace(/\\r/g, "\\r")
+					   .replace(/\\t/g, "\\t")
+					   .replace(/\\b/g, "\\b")
+					   .replace(/\\f/g, "\\f");
+	dataFile = encodeURIComponent(dataFile)
+	dataFile = decodeURIComponent(dataFile)
+	dataFile = JSON.parse(dataFile)
+
+	// let solution = JSON.parse(dataFile)
+	response.render("index.ejs", {
+		pageTitle: dataFile
+	})
+})
+
+
+app.get("/wlan", (request, response) => {
+	var dataFile = fs.readFileSync(path.join(__dirname, "./wlan/static/data/output.json"), 'utf8');
+	dataFile = JSON.stringify(dataFile)
+	dataFile = encodeURIComponent(dataFile)
+	dataFile = decodeURIComponent(dataFile)
+	dataFile = JSON.parse(dataFile)
+
+	// let solution = JSON.parse(dataFile)
+	response.render("index.ejs", {
+		pageTitle: dataFile
+	})
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Express server listening on port ${port}`)
+})
